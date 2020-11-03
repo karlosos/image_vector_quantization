e2520b223e97a105344e16c0ac8973cab7843018
Karol Dzialowski <karlososhd@gmail.com>
2020-11-03 18:56:59 +0100

    :sparkles: lbg algorithm

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import numpy as np
 from PIL import Image
+
 from vector_quantization.metrics import PSNR
 from vector_quantization.image import load_image, save_image
 from vector_quantization.codebooks import random_codebook
-from vector_quantization.quantization import quantize
+from vector_quantization.vectorize import vectorize, image_from_vectors
+from vector_quantization.quantization import quantize, quantize_from_codebook
+from vector_quantization.lbg import lbg
 
 if __name__ == "__main__":
-
     img = load_image("balloon.bmp")
-    quantized_img = quantize(img, window_size=4, codebook_fun=random_codebook, codebook_size=32)
-    Image.fromarray(quantized_img).show()
+    vectors = vectorize(img, window_size=4)
+    initial_codebook = random_codebook(vectors, length=32)
+    codebook, distortions = lbg(vectors, np.array(initial_codebook), 50, 0.01)
+    quantized_img_lbg = image_from_vectors(quantize_from_codebook(vectors, initial_codebook), img)
+    Image.fromarray(quantized_img_lbg).show()
+
+    print(codebook)
+    print(distortions)
