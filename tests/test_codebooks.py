fca2afd9b439ce3f2a0cab6f042fe3d6ae459f4d
Karol Dzialowski <karlososhd@gmail.com>
2020-12-01 19:58:13 +0100

    :alembic: consistent random codebooks between lbg and mrvq experiments

--- tests/test_codebooks.py
+++ tests/test_codebooks.py
@@ -15,3 +15,9 @@ class TestRandomCodebook(unittest.TestCase):
         self.assertTrue([2, 2] in codebook)
         self.assertTrue([3, 3] in codebook)
         self.assertTrue([4, 4] in codebook)
+
+    def test_seed_always_same(self):
+        vectors = np.array([[1, 1], [2, 2], [1, 1], [3, 3], [4, 4], [1, 1], [3, 3], [5, 5], [9, 9], [21, 21]])
+        codebook_1 = random_codebook(vectors, 4, seed=42)
+        codebook_2 = random_codebook(vectors, 4, seed=42)
+        npt.assert_almost_equal(codebook_1, codebook_2)
